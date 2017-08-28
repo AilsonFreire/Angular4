4d87e71e23f8d159b20889fb73647b0d386d4d7a
Ailson Freire <ailson_freire@outlook.com>
2017-08-27 23:44:24 -0300

    Configuração e teste de variaveis

---
+++ app.ts
@@ -0,0 +1,17 @@
+let message : string = "Help me Obi-wan Kenobi. You're my only hope";
+console.log( message );
+
+let episode : number = 4;
+console.log( "This is espisode " + 4);
+episode += 1;
+console.log("Next episode is " + episode) ;
+
+let favoriteDroid;
+
+favoriteDroid = "BB8";
+
+let shortestRun = function ( parsecs : number ) : boolean {
+  return parsecs < 21;
+}
+
+console.log(shortestRun(9));
---
+++ dist/app.js
@@ -0,0 +1,12 @@
+var message = "Help me Obi-wan Kenobi. You're my only hope";
+console.log(message);
+var episode = 4;
+console.log("This is espisode " + 4);
+episode += 1;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = "BB8";
+var shortestRun = function (parsecs) {
+    return parsecs < 21;
+};
+console.log(shortestRun(9));
